1fc7485f54bcb6458fdf93e0ce2b6fa5f184302a
Saddar U Din <saddarudin338@gmail.com>
2024-02-03 11:16:19 +0500

    Adding python codes of todays practiced problems

---
+++ Python/variables_practice.py
@@ -0,0 +1,28 @@
+# break = 5
+# above statement is incorrect because "break" is a reserved keyword in Python
+
+
+
+
+# Question2:
+# create two variables one to store your birth year and
+# one to store current year. Now calculate your age using these two variables
+
+birth_year = 2004
+current_year = 2024
+age = current_year-birth_year
+print(age)
+
+
+
+
+
+# Question3:
+# Store your first, middle and last name in three different variables
+# and print your name using these variables
+
+first = "Saddar"
+middle = "U Din"
+last = "Babar"
+
+print(first+" "+middle+" "+last)
