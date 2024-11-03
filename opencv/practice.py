386c3b375917563b1f7a5a1f22531c335f5cf9f1
Saddar U Din Babar <saddarudin338@gmail.com>
2024-11-03 14:57:04 +0500

    worked on screen recording and video downloadinig from youtube

--- opencv/practice.py
+++ opencv/practice.py
@@ -1,15 +1,1 @@
-import cv2 as cv
-
-cap = cv.VideoCapture(0)
-
-while cap.isOpened():
-    ret,frame = cap.read()
-    if not ret:
-        print("Webcam stopped")
-        break
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-    cv.imshow('webcam',frame)
-    
-cap.release()
-cv.destroyAllWindows()
+print('hi saddar')
